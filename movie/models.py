f6b46aef1068d5cf2cbdf01bcb85a97d8b766505
HMY777 <hhmy777@163.com>
2020-04-20 22:17:53 +0800

    导入数据
    导入了poster，info等数据。把user,movie,rating这几个主要的模型导入了。

--- movie/models.py
+++ movie/models.py
@@ -7,30 +7,34 @@ class Genre(models.Model):
 
     class Meta:
         db_table = 'Genre'
-
+    def __str__(self):
+        return f"<Genre:{self.name}>"
 
 class Movie(models.Model):
     # 电影名
     name = models.CharField(max_length=256)
-    # 海报
-    poster = models.FileField(max_length=256)
-    # 持续时间
-    duration = models.CharField(max_length=256)
+    # movie_id，用来对应static里面的海报
+    movie_id = models.CharField(max_length=256)
+    # 时长
+    time = models.CharField(max_length=256,blank=True)
     # 类型
     genre = models.ManyToManyField(Genre)
     # 发行时间
-    release_time = models.CharField(max_length=256)
+    release_time = models.CharField(max_length=256,blank=True)
     # 简介
-    intro = models.TextField()
+    intro = models.TextField(blank=True)
     # 导演
-    director = models.CharField(max_length=256)
+    director = models.CharField(max_length=256,blank=True)
     # 编剧
-    writers = models.CharField(max_length=256)
+    writers = models.CharField(max_length=256,blank=True)
     # 演员
-    actors = models.CharField(max_length=512)
+    actors = models.CharField(max_length=512,blank=True)
 
     class Meta:
         db_table = 'Movie'
+
+    def __str__(self):
+        return f"<Movie:{self.name},{self.movie_id}>"
 
 class User(models.Model):
     name = models.CharField(max_length=128, unique=True)
@@ -47,9 +51,9 @@ class User(models.Model):
 
 class Movie_rating(models.Model):
     # 评分的用户
-    uid = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,unique=False)
     # 评分的电影
-    mid = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE,unique=False)
     # 分数，范围是0.5~5,步长是0.5，用户不能出现1.3,1.4等评分
     score = models.FloatField(validators=[validators.MinValueValidator(0),validators.MaxLengthValidator(5)])
     # 评论，可选
--- movie/views.py
+++ movie/views.py
@@ -1,16 +1,98 @@
+import csv
+import os.path
 from django.shortcuts import render,redirect,reverse
 from django.views.generic import View
 from django.contrib import messages
 from .forms import RegisterForm,LoginForm
 from django.http import HttpResponse,request
-from .models import User,Movie
+from .models import User,Movie,Genre,Movie_rating
+# DO NOT MAKE ANY CHANGES
+BASE = os.path.dirname(os.path.abspath(__file__))
 
 
-# Create your views here.
+
+def get_genre():
+    '''导入所有电影类型'''
+    path=os.path.join(BASE,'static\movie\info\genre.txt')
+    with open(path) as fb:
+        for line in fb:
+            Genre.objects.create(name=line.strip())
+
+def get_movie_info():
+    '''导入所有电影信息，设置它们的类型'''
+    path=os.path.join(BASE,'static\movie\info\info.csv')
+    with open(path) as fb:
+        reader=csv.reader(fb)
+        title=reader.__next__()
+        # 读取title信息 id,name,url,time,genre,release_time,intro,directores,writers,starts
+        # 这里的id是imbd的id，根据它来访问static文件夹下面的poster
+        title_dct=dict(zip(title,range(len(title))))
+        # print(title_dct)
+        # print(path)
+        for i,line in enumerate(reader):
+            m=Movie.objects.create(name=line[title_dct['name']],
+                                 movie_id=line[title_dct['id']],
+                                 time=line[title_dct['time']],
+                                 release_time=line[title_dct['release_time']],
+                                 intro=line[title_dct['intro']],
+                                 director=line[title_dct['directors']],
+                                 writers=line[title_dct['writers']],
+                                 actors=line[title_dct['starts']])
+            # 必须要先保存才能建立关系
+            m.save()
+            # 建立类型关系
+            for genre in line[title_dct['genre']].split('|'):
+                # 找到类型 genre_object
+                go=Genre.objects.filter(name=genre).first()
+                # print(go)
+                m.genre.add(go)
+            if i%1000==0:
+                print(i)    # 控制台查看进度用
+            # pass
+
+def get_user_rating():
+    '''
+    获取ratings文件，设置用户信息和对电影的评分
+    由于用户没有独立的信息，默认用这种方式保存用户User: name=userId,password=userId,email=userId@1.com
+    通过movie_Id对电影进行关联，设置用户对电影的评分,comment默认为空
+    '''
+    path = os.path.join(BASE, r'static\movie\info\ratings.csv')
+    with open(path) as fb:
+        reader=csv.reader(fb)
+        # userId,movieId,rating,timestamp,timestamp不用管
+        title=reader.__next__()
+        title_dct=dict(zip(title,range(len(title))))
+        # csv文件中，一条记录就是一个用户对一部电影的评分和时间戳，一个用户可能有多条评论
+        # 所以要先取出用户所有的评分，设置成一个字典,格式为{ user:{movie1:rating, movie2:rating, ...}, ...}
+        user_id_dct=dict()
+        for line in reader:
+            user_id=line[title_dct['userId']]
+            movie_id=line[title_dct['movieId']]
+            rating=line[title_dct['rating']]
+            user_id_dct.setdefault(user_id,dict())
+            user_id_dct[user_id][movie_id]=rating
+    # 对所有用户和评分记录
+    for user_id,ratings in user_id_dct.items():
+        u=User.objects.create(name=user_id,password=user_id,email=f'{user_id}@1.com')
+        # 必须先保存
+        u.save()
+        # 开始加入评分记录
+        for movie_id,rating in ratings.items():
+            # Movie_rating(uid=)
+            movie=Movie.objects.get(movie_id=movie_id)
+            relation=Movie_rating(user=u,movie=movie,score=rating,comment='')
+            relation.save()
+            # break
+        print(f'{user_id} process success')
+        # break
 
 def index(request):
-    # return HttpResponse("ok")
-    return render(request, 'movie/index.html')
+    # get_genre()
+    # get_movie_info()
+    # get_user_rating()
+    context={'movie':Movie.objects.filter(name="Toy Story (1995) ").first()}
+    # print(Movie.objects.filter(name="Toy Story (1995) ").first())
+    return render(request, 'movie/index.html',context=context)
 
 # 注册视图
 class RegisterView(View):
